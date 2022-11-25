9776cdbb83c610c0c85317dfa0f4f543a89a5858
Leonardo <78977800+JaumVituu@users.noreply.github.com>
2022-11-25 11:29:38 -0300

    A

--- Projeto TCC/Assets/Scripts/Inimigo.cs
+++ Projeto TCC/Assets/Scripts/Inimigo.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
 
+
 public class Inimigo : MonoBehaviour
 {
     [SerializeField]Slider BarraVida;
     [SerializeField]GameObject CanvasBarraVida; 
     //[SerializeField]NavMeshAgent agent;
     [SerializeField]GameObject ObjetoTextoDano;
-    [SerializeField]Text textoDano;
- 
+    Text textoDano;
+    [SerializeField]NavMeshAgent agente;
+    Camera cameraAtiva;
     int vida;
     [SerializeField]int vidaInicial;
+    List<GameObject> textos = new List<GameObject>();
     void Start()
     {
         vida = vidaInicial;
@@ -23,8 +26,10 @@ public class Inimigo : MonoBehaviour
     void Update()
     {
         BarraVida.value = vida;
-        Camera cameraAtiva = (Camera)FindObjectOfType(typeof(Camera));
+        cameraAtiva = (Camera)FindObjectOfType(typeof(Camera));
         CanvasBarraVida.transform.LookAt(cameraAtiva.transform);
+        agente.SetDestination(GameObject.FindWithTag("Player").transform.position);
+
     }
 
     public void PerderVida(TipoDano danoRecebido){
@@ -34,24 +39,52 @@ public class Inimigo : MonoBehaviour
         if(colisorAtingido.name == "Head hurtbox"){
             vidaPerdida = dano*3;
             vida -= vidaPerdida;
-            StartCoroutine(MostrarTextoDano(vidaPerdida));
+            if(vida > 0){
+                MostrarTextoDano(vidaPerdida);
+            }
         }
         if(colisorAtingido.name == "Body hurtbox"){
             vidaPerdida = dano;
             vida -= vidaPerdida;
-            StartCoroutine(MostrarTextoDano(vidaPerdida));
+            if(vida > 0){
+                MostrarTextoDano(vidaPerdida);
+            }
         }       
         if (vida <= 0){
+            /*foreach(i in textos){
+                Destroy(textos[i]);
+            };*/
             Destroy(gameObject);
         }
     }
 
-    IEnumerator MostrarTextoDano(int danoTexto){
-        GameObject textoDano = Instantiate(ObjetoTextoDano, transform.position + new Vector3(2,0,0),Quaternion.identity);    
+    void MostrarTextoDano(int danoTexto){
+        GameObject textoDano = Instantiate(ObjetoTextoDano, transform.position + new Vector3(0,3f,0),Quaternion.identity);
+        textoDano.transform.LookAt(cameraAtiva.transform);    
         Text texto = textoDano.transform.GetChild(0).GetComponent<Text>();
         texto.text = danoTexto.ToString();
-        textoDano.transform.Translate(0,1*Time.deltaTime,0);
-        yield return new WaitForSeconds(3);
-        Destroy(textoDano);
+        textoDano.GetComponent<Rigidbody>().velocity = new Vector3(0,2,0);
+        StartCoroutine(FadeText(texto.color,textoDano));
     }
+
+    IEnumerator FadeText(Color textoFade, GameObject objetoTexto){
+        float i = 1;
+        while(i>=0){
+            if(vida<=0){
+                Destroy(textoDano,0.25f);
+            }
+            textoFade = new Color (textoFade.r,textoFade.g,textoFade.b,i);
+            i -= 0.1f;
+            yield return new WaitForSeconds(0.05f);
+        }
+        if(i<=0){
+            Destroy(objetoTexto);
+        }
+    }
+
+    /*void ChecarParedes(){
+        if(Physics.SphereCast()){
+
+        }
+    }*/
 }
